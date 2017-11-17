d65cce2157eb751aef157f7eb5d40f6bbd2e2964
Sudipta Paul <sudiptapaul1978@gmail.com>
2017-11-17 07:57:55 -0800

    Modified to avoid WSO2 calling in Dev mode

--- src/app/_interceptor/http.requests.interceptor.ts
+++ src/app/_interceptor/http.requests.interceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable, isDevMode} from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
@@ -14,42 +14,56 @@ export class HttpRequestsInterceptor implements HttpInterceptor {
   constructor(private tokenGeneratorService: TokenGeneratorService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('isDevMode() > ' + isDevMode());
     let appToken = this.tokenGeneratorService.getToken();
     console.log('appToken in Local Storage > ' + JSON.stringify(appToken));
-    const lastAppTokenExprires = appToken.expiresIn;
-    const tokenGenerationTime = appToken.tokenGenerationTime;
-    const currentTimeInSeconds = Math.round((Date.now()) / 1000);
-    const timeSpendFromLastTokenGeneration = (currentTimeInSeconds - tokenGenerationTime);
-    const timeRemaining = (lastAppTokenExprires - timeSpendFromLastTokenGeneration);
-    console.log('lastAppTokenExprires > ' + lastAppTokenExprires);
-    console.log('tokenGenerationTime > ' + tokenGenerationTime);
-    console.log('currentTimeInSeconds > ' + currentTimeInSeconds);
-    console.log('timeSpendFromLastTokenGeneration > ' + timeSpendFromLastTokenGeneration);
-    console.log('timeRemaining > ' + timeRemaining);
+    if (!isDevMode()) {
+      // go for WSO2 token generation
+      console.log('appToken in Local Storage > ' + JSON.stringify(appToken));
+      const lastAppTokenExprires = appToken.expiresIn;
+      const tokenGenerationTime = appToken.tokenGenerationTime;
+      const currentTimeInSeconds = Math.round((Date.now()) / 1000);
+      const timeSpendFromLastTokenGeneration = (currentTimeInSeconds - tokenGenerationTime);
+      const timeRemaining = (lastAppTokenExprires - timeSpendFromLastTokenGeneration);
+      console.log('lastAppTokenExprires > ' + lastAppTokenExprires);
+      console.log('tokenGenerationTime > ' + tokenGenerationTime);
+      console.log('currentTimeInSeconds > ' + currentTimeInSeconds);
+      console.log('timeSpendFromLastTokenGeneration > ' + timeSpendFromLastTokenGeneration);
+      console.log('timeRemaining > ' + timeRemaining);
 
-    if (appToken === null || appToken.accessToken === '' || timeRemaining < 5) {
-      this.tokenGeneratorService.getTokenFromWSO2().subscribe( resp => {
-        appToken = resp;
-        this.tokenGeneratorService.setToken(appToken);
+      if ((appToken === null || appToken.accessToken === '' || timeRemaining < 5)) {
+        this.tokenGeneratorService.getTokenFromWSO2().subscribe( resp => {
+          appToken = resp;
+          this.tokenGeneratorService.setToken(appToken);
+        });
+      }
+
+      request = request.clone({
+        setHeaders: {
+          'Authorization': `Bearer ${appToken.accessToken}`,
+          'Content-Type': 'application/json'
+        }
+      });
+    } else {
+      // this is development so no need to set the WSO2 token for the API access
+      request = request.clone({
+        setHeaders: {
+          'Content-Type': 'application/json'
+        }
       });
     }
-    request = request.clone({
-      setHeaders: {
-        'Authorization' : `Bearer ${appToken.accessToken}`,
-        'Content-Type': 'application/json'
-      }
-    });
+
     return next.handle(request).do((event: HttpEvent<any>) => {
       if (event instanceof HttpResponse) {
-        console.log('httpReponse is good');
+        console.log('Reponse is good');
       }
     }, (err: any) => {
       if (err instanceof HttpErrorResponse) {
         if (err.status === 401) {
-          console.log('unauthorized response');
+          console.log('Unauthorized response');
           this.tokenGeneratorService.getTokenFromWSO2()
             .subscribe(resp => {
-              console.log('got token in authservice:' + resp);
+              console.log('Got the token in authservice:' + resp);
               this.tokenGeneratorService.setToken(resp);
             });
         }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,11 @@
 
 export const environment = {
   production: false,
-  // userURL: 'http://localhost:9080/user',
-  // policyURL: 'http://localhost:9082/policies'
+  userURL: 'http://localhost:9080/user',
+   policyURL: 'http://localhost:9082/policies',
+  /*
   userURL: 'https://gateway.api.cloud.wso2.com:443/t/cts8650/users/1.0.0',
-  policyURL: 'https://gateway.api.cloud.wso2.com:443/t/cts8650/policies/1.0.0',
-  tokenURL: 'https://cors-anywhere.herokuapp.com/https://gateway.api.cloud.wso2.com:443/token',
-  tokenAuthKey: 'Basic RkdUY1lLZktxUUxEaDczY0gzR0NzTFJabkRJYTp4Q3hCUFk3Rm5neUU4Wmk4VjF4MTNQekIxMlVh'
-
+  policyURL: 'https://gateway.api.cloud.wso2.com:443/t/cts8650/policies/1.0.0',*/
+  tokenURL: '',
+  tokenAuthKey: ''
 };
